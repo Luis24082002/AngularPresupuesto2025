82e79fc4cff2e39c90eb7932752164961e165a12
Luis Carlos Duarte Gonzalez <luisdu0824@gmail.com>
2025-02-07 15:56:07 -0600

    Configuracion de Pagina Finanzas2025

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { RouterLinkWithHref, Routes } from '@angular/router';
+import { IngresosComponent } from './Paginas/ingresos/ingresos.component';
+import { GastosComponent } from './Paginas/gastos/gastos.component';
+
+
+
+export const routes: Routes = [
+{path: 'ingresos',component: IngresosComponent},
+{path: 'gastos', component: GastosComponent}
+];
